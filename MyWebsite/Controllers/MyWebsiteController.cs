a0f1dc610f00fc38ac12173ca9d0ab2d7702d42b
danielastateresa <149039463+danielastateresa@users.noreply.github.com>
2025-06-07 17:30:08 +0800

    Simple Web App with MVC

---
+++ MyWebsite/Controllers/MyWebsiteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebsite.Controllers
+{
+    public class WebsiteController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
